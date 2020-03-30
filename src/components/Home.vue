0eaa3e13daa5240fc49d5ffbcdb1127f9cefe649
Yoruroong <tuntun4132@naver.com>
2020-03-31 00:33:50 +0900

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,37 @@
+<template>
+<v-container>
+    <v-layout row wrap>
+        <v-flex xs6>
+            <List/>
+        </v-flex>
+        <v-flex xs6>
+            <ListAdd
+                @listAdd="listAdd"
+            />
+        </v-flex>
+    </v-layout>
+</v-container>
+</template>
+
+<script>
+import List from './List'
+import ListAdd from './ListAdd'
+
+export default {
+    components: {
+        List,
+        ListAdd
+    },
+    data() {
+        return {
+            todoList: []
+        }
+    },
+    methods: {
+        listAdd(memo) {
+            console.log("받음!" + memo)
+            this.todoList.push(memo)
+        }
+    }
+}
+</script>
---
+++ src/components/ListAdd.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <v-textarea
+          outlined
+          v-model="memo"
+          label="투두리스트를 입력해주세요"
+          value=""
+        ></v-textarea>
+        <v-btn @click="listAdd">리스트 추가</v-btn>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            memo: null
+        }
+    },
+    methods: {
+        listAdd() {
+            console.log('리스트 추가')
+            if(this.memo === null){
+                alert('저기요?')
+            }
+            this.$emit('listAdd', this.memo)
+        }
+    }
+}
+</script>
